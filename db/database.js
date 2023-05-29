adb897de4a64aa9ec07888dd580bb543b9587dd2
Nnaemeka Bethrand <bethrand2019@gmail.com>
2023-05-29 22:51:18 +0100

    Added DB

--- db/database.js
+++ db/database.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const LOCAL_DB =
   `mongodb://0.0.0.0:27017/saveply` || `mongodb://localhost:27017/saveply`;
 
+const DB = `mongodb+srv://Bee:${process.env.DBPASSWORD}@practise-cluster.bx9s68v.mongodb.net/`;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(LOCAL_DB);
+    await mongoose.connect(DB);
     console.log("Connected to SavePly db");
   } catch (err) {
     console.error(err);
